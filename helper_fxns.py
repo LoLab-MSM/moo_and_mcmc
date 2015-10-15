fab486acdc63e03e87a5160b40a770667e1d2c9e
Erin Shockley <erin.shockley@vanderbilt.edu>
2015-10-15 14:27:44 -0500

    Fixing gelman rubin convergence fxn if param_list key is present.

--- helper_fxns.py
+++ helper_fxns.py
@@ -16,6 +16,10 @@ import os
 import dill
 
 def gelman_rubin_trace_dict(trace_dict, burnin=0):
+    try:
+        trace_dict.pop('param_list')
+    except KeyError:
+        pass
     Rhat = {}
     def calc_rhat(var_dict):
         # a ValueError that will handle the multidimensional case
